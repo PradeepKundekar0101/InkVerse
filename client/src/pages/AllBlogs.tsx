0c1a97607e0c155aad06480343ba58b7d4e59b61
Pradeep Kundekar <pradeepkundekar1010@gmail.com>
2024-01-09 11:11:21 +0530

    modified allBlog.tsx

--- client/src/pages/AllBlogs.tsx
+++ client/src/pages/AllBlogs.tsx
@@ -20,21 +20,27 @@ const AllBlogs = () => {
     }
     const [pageNo,setPageNo]= useState<number>(1);
     const [totalPages,setTotalPages] = useState<number>(0);
-    const [blogs,setBlogs] = useState<Blog[]|null>(null);
+    const [blogs,setBlogs] = useState<Blog[]>([]);
+    const [loading,setLoading] = useState<boolean>(true);
+    const [error,setError] = useState<string | null>(null);
     const fetchBlogs =async ()=>{
         try {
+            setLoading(true);
+            setError(null);
             const url = `${serverUrl}/api/blog/all?page=${pageNo}`
             const response = await axios.get(url);
             setBlogs(response.data.data);
             setTotalPages(Math.ceil(response.data.totalDocs/10))
-        } catch (error) {
-            console.log(error)
+            setLoading(false)
+        } catch (error:any) {
+            setLoading(false);
+            setError(error.message);
         }
     }
     
     const sortBlog = async (parameter:string)=>{
             try {
-                setBlogs(null);
+                setBlogs([]);
                 const url = `${serverUrl}/api/blog/all?sort=${parameter}&page=${pageNo}`
                 const response = await axios.get(url);
                 setBlogs(response.data.data);
@@ -43,17 +49,20 @@ const AllBlogs = () => {
             }
     }
     useEffect(() => {
-        setBlogs(null);
+        setBlogs([]);
         fetchBlogs();
     }, [pageNo])
-    
+    if(loading) return <AllBlogLoader/>
+    if(error) return <div className='h-screen text-center flex items-center justify-center dark:bg-black'>
+        <h1 className='text-4xl dark:text-white '>{error}</h1> 
+    </div>
   return (
     <div className='bg-slate-100 dark:bg-gray-950 dark:text-white'>
         <h1 className='text-3xl text-center font-medium py-4 dark:text-white lg:text-5xl lg:py-10'>All Blogs</h1>
             <SearchBlogForm/>
             <SortByBtn sortBlog={sortBlog}/>
 
-        {!blogs? <AllBlogLoader/> :
+        {
             <div className='container mx-auto'>
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {blogs.map((blog,ind)=>(
